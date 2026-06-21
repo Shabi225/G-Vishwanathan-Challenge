4b66d7c0a779656cd5d2bbee5300ae43c9465e41
Shabi Zehra <shabi81769@gmail.com>
2026-06-21 18:20:32 +0530

    Day1: method to count numbers with even number of digits

--- Day1.java
+++ Day1.java
@@ -1,4 +1,4 @@
-/*Given an array nums of integers, return how many of them contain an even number of digits.*/
+/*Given an array named nums of integers, return how many of them contain an even number of digits.*/
 
 class Solution {
     public int findNumbers(int[] nums) {
