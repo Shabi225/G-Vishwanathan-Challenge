d0b143b2b7da4e72b1e017d65ef5d1389b1c9c84
Shabi Zehra <shabi81769@gmail.com>
2026-07-10 17:20:55 +0530

    Day41: Refactor code for CalculatingFunction_486A

--- Day41(2).java
+++ Day41(2).java
@@ -1,25 +1,15 @@
+/*Codeforces 486A*/
 import java.util.Scanner;
 
-public class cAPSlOCK_131A {
+public class CalculatingFunction_486A {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        String word=sc.next();
-        StringBuilder ans=new StringBuilder();
-        char first=word.charAt(0);
-        String rest=word.substring(1);
-        if((Character.isLowerCase(first) && rest.toUpperCase().equals(rest))){
-            ans.append(Character.toUpperCase(first));
-            ans.append(rest.toLowerCase());
-            System.out.println(ans.toString());
+        long n=sc.nextLong();
+        if(n%2==0){
+            System.out.println(n/2);
         }
-        else if (word.toUpperCase().equals(word)) {
-            ans.append(word.toLowerCase());
-            System.out.println(ans.toString());
+        else{
+            System.out.println(-((n/2)+1));
         }
-        else {
-            ans.append(word);
-            System.out.println(ans.toString());
-        }
-
     }
 }
