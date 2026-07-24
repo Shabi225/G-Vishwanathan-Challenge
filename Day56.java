bd05a9dc4c3b43b9258163b2a8ec5e4cc1576d50
Shabi Zehra <shabi81769@gmail.com>
2026-07-24 22:53:48 +0530

    Day56: Implement Even-Odds solution for Codeforces 318A

---
+++ Day56.java
@@ -0,0 +1,17 @@
+/*Codeforces318A*/
+import java.util.Scanner;
+
+public class EvenOdds_318A {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        long n=sc.nextLong();
+        long k=sc.nextLong();
+        long oddCount=(n+1)/2;
+        if(k<=oddCount){
+            System.out.println(2*k-1);
+        }
+        else {
+            System.out.println(2*(k-oddCount));
+        }
+    }
+}
