a25aca9ba05241b17dde65e47521c704b923bbb1
Shabi Zehra <shabi81769@gmail.com>
2026-08-04 18:41:21 +0530

    Day66: Add KyoyaAndPhotobooks_554A implementation

---
+++ Day66.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class KyoyaAndPhotobooks_554A {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s= sc.next();
+        int l=s.length();
+        System.out.println((26*(l+1))-l);
+    }
+}
