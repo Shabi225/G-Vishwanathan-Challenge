b00dd9b97611cf5b2f92e952d7dbfeae692215be
Shabi Zehra <shabi81769@gmail.com>
2026-08-18 15:23:12 +0530

    Day72: Implement recursive method to reverse a linked list

---
+++ Day72.java
@@ -0,0 +1,18 @@
+class Solution {
+    public ListNode solve(ListNode prev,ListNode curr){
+        if(curr==null){
+            return prev;
+        }
+        ListNode forward=curr.next;
+        curr.next=prev;
+        prev=curr;
+        curr=forward;
+        return solve(prev,curr);
+    }
+    public ListNode reverseList(ListNode head) {
+        ListNode prev=null;
+        ListNode curr=head;
+        ListNode ans=solve(prev,curr);
+        return ans;
+    }
+}
